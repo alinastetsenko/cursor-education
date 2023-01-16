c9b3460ee3031c2038d5277cf1b940a1466e13e0
Alina Atetsenko <alinastatsenko1406@gmail.com>
2023-01-16 16:36:01 +0200

    add 6 hw all files

---
+++ hw6/js/slider.js
@@ -0,0 +1,9 @@
+$(".slider-first").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    adaptiveHeight: true,
+});
